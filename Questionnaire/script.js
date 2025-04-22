9add758880321bc9a506ffb9b006b64159c0d19b
Patrick Jolo <87263053+pjolo@users.noreply.github.com>
2025-04-22 13:38:37 +0200

    Update script.js

--- Questionnaire/script.js
+++ Questionnaire/script.js
@@ -1,1 +1,60 @@
+// This is the form definition
+var formDef = {
+  "status": "draft",
+  "title": "Demo form",
+  "resourceType": "Questionnaire",
+  "item": [
+    {
+      "type": "group",
+      "linkId": "808145178143",
+      "text": "Basic Information"
+    },
+    {
+      "type": "string",
+      "linkId": "900843292121",
+      "text": "Name"
+    },
+    {
+      "type": "integer",
+      "linkId": "628859034495",
+      "text": "Age"
+    },
+    {
+      "type": "dateTime",
+      "linkId": "854205526824",
+      "text": "Appointment Date and Time"
+    },
+    {
+      "type": "group",
+      "linkId": "/X123",
+      "text": "Family member",
+      "item": [
+        {
+          "type": "string",
+          "linkId": "/X123/X124",
+          "text": "Name"
+        },
+        {
+          "type": "group",
+          "repeats": true,
+          "linkId": "/X123/X125",
+          "text": "Surgical History",
+          "item": [
+            {
+              "type": "date",
+              "linkId": "/X123/X125/X126",
+              "text": "Date"
+            },
+            {
+              "type": "string",
+              "linkId": "/X123/X125/X127",
+              "text": "Surgery type"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+LForms.Util.addFormToPage(formDef, 'formContainer');
 
